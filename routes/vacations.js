2aebab3cae07084607e019d7026ef4f881ed09f4
claudiuilie <claudiu.ilie0322@gmail.com>
2020-09-25 16:43:52 +0300

    my_plate

--- routes/vacations.js
+++ routes/vacations.js
@@ -6,12 +6,42 @@ const config = new options();
 let router = express.Router();
 let mysql = new mysqlController(config.mysql);
 
-router.get('/',(req,res,next) => {
+router.get('/', (req, res, next) => {
 
     if (!req.session.loggedin) res.redirect('/auth');
 
     else {
-        res.render('vacations',{});
+
+        let mysql = new mysqlController(config.mysql);
+
+        mysql.select('my_plate', { 1: 1 }, (error, results) => {
+            console.log(results);
+            let items = {
+                proteine: [],
+                legume: [],
+                carbohidrati: [],
+                grasimi: []
+            };
+
+            for (let k in results) {
+                switch (results[k].category) {
+                    case "Proteine":
+                        items.proteine.push(results[k])
+                        break;
+                    case "Legume":
+                        items.legume.push(results[k])
+                        break;
+                    case "Carbohidrati":
+                        items.carbohidrati.push(results[k])
+                        break;
+                    case "Grasimi":
+                        items.grasimi.push(results[k])
+                        break;
+                }
+            }
+
+            res.render('vacations', { item: items });
+        });
     }
 });
 
