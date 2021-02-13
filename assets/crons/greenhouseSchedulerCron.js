37272c283fcf7965b5c176b7c37e3e4b20e8b7a3
Ilie Claudiu <claudiu.ilie0322@gmail.com>
2021-02-13 09:33:16 +0200

    controller

--- assets/crons/greenhouseSchedulerCron.js
+++ assets/crons/greenhouseSchedulerCron.js
@@ -10,47 +10,47 @@ let currentDate;
 
 class greenhouseSchedulerCron {
     constructor() {
- 
-        cron.schedule('00 0,10,15,20,25,30,35,40,45,50,55 0-23 * * *', () => {
+        // '00 0,10,15,20,25,30,35,40,45,50,55 0-23 * * *'
+        cron.schedule('10,20,30,40,50 * * * * *', () => {
 
             console.log("cron scheduler");
             request(config.arduino.host, (error, response, body) => {
 
                 if (error) {
                     console.log(error)
-                }else{
+                } else {
                     let responseBody = JSON.parse(body);
                     let sensors = responseBody.variables;
 
-                     if(sensors.fan_in == 0){
-                         startInFan(100)
-                     }
+                    if (sensors.fan_in <= 1) {
+                        startInFan(128)
+                    }
 
-                    if(sensors.temperature > 26 || sensors.humidity > 70){
-                      if(sensors.fan_in <= 230){
-                       startInFan(sensors.fan_in + 25)
-                      }else if(sensors.fan_in > 230 && sensors.fan_in < 255){
-                       startInFan(255)
-                      }
-                    }else if(sensors.temperature <= 26 && sensors.temperature >24 && sensors.humidity < 70){
-                      startFanIn(sensors.fan_in - 25)
-                    }else{startFanIn(100)}
+                    if (sensors.temperature > 26 || sensors.humidity >= 70) {
+                        if (sensors.fan_in <= 230) {
+                            startInFan(sensors.fan_in + 25)
+                        } else if (sensors.fan_in > 230 && sensors.fan_in < 255) {
+                            startInFan(255)
+                        }
+                    } else if (sensors.temperature <= 26 && sensors.temperature > 24 && sensors.humidity < 70) {
+                        startInFan(153)
+                    } else {
+                        startInFan(128)
+                    }
+
                     validateLampPhase();
-                    console.log(sensors);
                 }
             })
         });
 
-        function validateLampPhase(){
-            
+        function validateLampPhase() {
+
             mysql.select('greenhouse_schedule', { "active": 1 }, async (error, results) => {
                 if (error) {
                     console.log(error);
                 } else {
-                    console.log(results);
-    
                     let resultBody = results[0];
-    
+
                     if (resultBody.veg_phase) {
                         let activeHours = await checkHours(resultBody.lamp_start, resultBody.lamp_stop);
                         startLamp("vegPhase", activeHours);
@@ -66,7 +66,7 @@ class greenhouseSchedulerCron {
             function checkHours(startHour, stopHour) {
                 let date = new DateAndTime();
                 let hours = date.getHours();
-    
+
                 if (hours < startHour || hours >= stopHour) {
                     return false;
                 } else if (hours == stopHour) {
@@ -75,30 +75,30 @@ class greenhouseSchedulerCron {
                     return true;
                 }
             }
-    
+
             function startLamp(type, active) {
                 console.log(active)
                 if (active) {
                     request(`${config.arduino.host}/${type}?params=1`, (error, response, body) => {
-    
+
                         if (error) {
                             console.log(error)
                         }
-    
+
                         console.log(body);
                     })
                 } else {
                     stopLamp();
                 }
             }
-    
+
             function stopLamp() {
                 request(`${config.arduino.host}/vegPhase?params=0`, (error, response, body) => {
-    
+
                     if (error) {
                         console.log(error)
                     }
-    
+
                     console.log(body);
                 });
 
@@ -106,6 +106,8 @@ class greenhouseSchedulerCron {
         }
 
         function startInFan(value) {
+            console.log("Fan in set to: " +value);
+
             request(`${config.arduino.host}/fanIn?params=${value}`, (error, response, body) => {
 
                 if (error) {
